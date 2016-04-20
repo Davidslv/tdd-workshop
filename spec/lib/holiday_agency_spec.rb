64687630dd1b0061eb0d3679449a3463740eee91
David Silva <david.silva@digital.cabinet-office.gov.uk>
2016-04-20 08:30:55 +0100

    Add Holiday Agency spec

---
+++ spec/lib/holiday_agency_spec.rb
@@ -0,0 +1,26 @@
+describe 'Holiday Agency' do
+
+  context 'when villas are available' do
+    context 'and flights are not available' do
+      it 'returns no villas' do
+
+      end
+    end
+  end
+
+  context 'when flights are available' do
+    context 'and villas are not available' do
+      it 'returns no villas' do
+
+      end
+    end
+  end
+
+  context 'when villas are available' do
+    context 'and flights are available' do
+      it 'returns available villas' do
+
+      end
+    end
+  end
+end
